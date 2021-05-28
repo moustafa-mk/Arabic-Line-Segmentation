24ca65a4e77632432ef71d6a688385de87f546ed
a4w <ahmed.wessam.1999@gmail.com>
2021-05-28 14:44:05 +0200

    Convert to black and white and reverse dilation and erosion

--- main.py
+++ main.py
@@ -16,8 +16,11 @@ def dilate(img):
 if __name__ == '__main__':
     img_src = "OCR samples/sample2.png"
     img = cv.imread(img_src)
-    eroded = erode(img)
-    cv.imshow("erosion", eroded)
-    dilated = dilate(eroded)
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = cv.bitwise_not(img)
+    cv.imshow("not", img)
+    dilated = dilate(img)
     cv.imshow("dilation", dilated)
+    eroded = erode(dilated)
+    cv.imshow("erosion", eroded)
     cv.waitKey()
